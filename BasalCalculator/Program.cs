a21a61f542a1a19a0cf4ffbecb46b95d21608e0e
Jean Pablo Carvalho da Silva <jpablo1026@hotmail.com>
2022-10-24 21:27:38 -0300

    creating the sexValidator and starting the principal program

--- BasalCalculator/Program.cs
+++ BasalCalculator/Program.cs
@@ -1,13 +1,49 @@
 ﻿using System;
+using System.Linq.Expressions;
+using System.Runtime.CompilerServices;
+using BasalCalculator.Entities;
+using BasalCalculator.Validator;
 
 namespace BasalCalculator
 {
     class Program
     {
-        static void main(string[] args)
+        static void Main(string[] args)
         {
+            
+            SexValidator sexValidator = new SexValidator();
+            Console.Write("Qual o seu sexo? (M/F): ");
+            char sex = char.Parse(Console.ReadLine());
 
 
+            while (!sexValidator.ValidarSexo(sex))
+            {
+                Console.WriteLine("Resposta inválida! Por favor digite seu sexo: M = Masculino, F = Feminino");
+                Console.Write("Qual o seu sexo? (M/F): ");
+                sex = char.Parse(Console.ReadLine());
+            }
+
+
+            Console.Write("Qual o seu peso? ");
+            double weight = double.Parse(Console.ReadLine());
+            Console.Write("Qual a sua altura? (em cm) ");
+            int height = int.Parse(Console.ReadLine());
+            Console.Write("Qual a sua idade? ");
+            int age = int.Parse(Console.ReadLine());
+
+
+            if (sexValidator.ValidarSexoMasculino(sex))
+            {
+                Man man = new Man(age, weight, height);
+                int response = man.Calculator(age, weight, height);
+                Console.WriteLine($"Seu gasto calórico diário é de {response}");
+            }
+            else if (sexValidator.ValidarSexoFeminino(sex))
+            {
+                Woman woman = new Woman(age, weight, height);
+                int response = woman.Calculator(age, weight, height);
+                Console.WriteLine($"Seu gasto calórico diário é de {response}");
+            }
 
         }
     }
---
+++ BasalCalculator/Validator/SexValidator.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasalCalculator.Validator
+{
+    public class SexValidator
+    {
+        public char sex { get; set; }
+
+        public bool ValidarSexo(char sex)
+        {
+            if (sex != 'm' &&
+                sex != 'M' &&
+                sex != 'f' &&
+                sex != 'F')
+            {
+                return false;
+            }
+            else { return true; }
+
+        }
+
+        public bool ValidarSexoMasculino(char sex)
+        {
+            if (sex != 'm' &&
+                sex != 'M')
+            {
+                return false;
+            }
+            else { return true; }
+        }
+
+        public bool ValidarSexoFeminino(char sex)
+        {
+            if (sex != 'f' &&
+                sex != 'F')
+            {
+                return false;
+            }
+            else { return true; }
+        }
+
+    }
+}
